00432cc77baa076b662b102cbab732a8aee051b8
Your Name <jonathan.r.gilbert@gmail.com>
2018-03-26 00:08:13 -0700

    skeleton done, needs editing

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
 var express = require("express");
 var bp = require("body-parser");
+var app = express();
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burger_controller.js");
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+app.use(bp.urlencoded({ extended: true }));
+
+app.use(bp.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
